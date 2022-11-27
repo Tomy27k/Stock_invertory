2cef885e80e3fec394cd2b307a4b1cab258a6241
Halil Toma <tomahalil1@gmail.com>
2022-11-28 01:04:59 +0300

    edit footer

--- src/View/client/include/Footer.js
+++ src/View/client/include/Footer.js
@@ -2,17 +2,17 @@ import React from 'react'
 import { MapContainer, TileLayer, useMap ,Popup,Marker} from 'react-leaflet'
 const Footer = () => {
   return (
-    <footer className='d-flex '>
+    <footer className='d-flex flex-wrap'>
       <div className="footer-contact-info col-lg-6">
-        
+        sadasdasdasd
       </div>
       <div className="footer-map col-lg-6">
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer center={[33, 33]} zoom={13} scrollWheelZoom={false}>
   <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
-  <Marker position={[51.505, -0.09]}>
+  <Marker position={[37,37]}>
     <Popup>
       A pretty CSS3 popup. <br /> Easily customizable.
     </Popup>
